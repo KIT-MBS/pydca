160ec759c5581dede0d849a88c114f2e1dcb505e
Oskar Taubert <oskar.taubert@kit.edu>
2020-09-08 11:27:23 +0200

    added consistency test for more flexible interface

--- tests/meanfield_dca_test.py
+++ tests/meanfield_dca_test.py
@@ -3,6 +3,8 @@ from pydca.meanfield_dca.meanfield_dca import MeanFieldDCA
 from .input_files_path import InputFilesPath
 import unittest
 
+from Bio import AlignIO
+
 class MeanFieldDCATestCase(unittest.TestCase):
     """Test MeanFieldDCA instance behaviour
     """
@@ -35,6 +37,29 @@ class MeanFieldDCATestCase(unittest.TestCase):
         """
         """
         sorted_DI = self.__mfdca_instance_protein.compute_sorted_DI()
+
+
+class MeanFieldDCAInputTestCase(unittest.TestCase):
+    """
+    Consistency test for decoupling msa handling from msa file format
+    """
+    def setUp(self):
+        self.__protein_msa_file = InputFilesPath.protein_msa_file
+        self.__protein_ref_file = InputFilesPath.protein_ref_file
+
+    def test_input(self):
+        mfdca_file = MeanFieldDCA(self.__protein_msa_file, 'protein')
+        fnapc_file = mfdca_file.compute_sorted_FN_APC()
+
+        # read MSA
+        msa = AlignIO.read(self.__protein_msa_file, 'fasta')
+
+
+        mfdca = MeanFieldDCA(msa, 'protein')
+        fnapc = mfdca.compute_sorted_FN_APC()
+
+        self.assertEqual(fnapc, fnapc_file)
+
 
 if __name__ == '__main__':
     unittest.main()
